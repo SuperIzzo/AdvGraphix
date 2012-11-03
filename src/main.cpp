1d4b4d1e4985acc74c75369a62ddb793a2a719ac
Izzo <hristoz2004@gmail.com>
2012-11-03 10:36:25 +0000

    Added a basic window 'hack' and an execution option '-test' to run only tests...

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,22 @@
 #include <cstdio>
 
 
+int RunWnd();
+
 int main(int argc, char* args[])
 {
-	int result = 0;
+	if ( argc > 1   &&   strcmp("-test", args[1])==0 )
+	{
+		int result = 0;
 
-	printf("\n--=-=-=-==== Test ====-=-=-=--\n");
-    result = UnitTest::RunAllTests();
-	printf( "--=-=-=-== = == == == = ==-=-=-=--\n\n");
+		printf("\n--=-=-=-==== Test ====-=-=-=--\n");
+		result = UnitTest::RunAllTests();
+		printf( "--=-=-=-== = == == == = ==-=-=-=--\n\n");
 
-	return result;
+		return result;
+	}
+	else
+	{
+		return RunWnd();
+	}
 }
