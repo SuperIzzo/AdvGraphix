42ad359e4c206e4f39125a73c9873cd129122617
Hristoz Stefanov <hristoz2004@gmail.com>
2012-11-08 14:56:18 +0000

    Graphics3D now renders more ineficiently, but is easier to use

--- include/Graphics3D.h
+++ include/Graphics3D.h
@@ -6,25 +6,34 @@
 #ifndef __GRAPHICS_3D_H__
 #define __GRAPHICS_3D_H__
 
-#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/Color.hpp>
 
 #include "Matrix.h"
 #include "Vector.h"
 
 
-class Graphics3D : public sf::Drawable
+class Graphics3D
 {
 public:
-	void				Line( Vector3f p1,  Vector3f p2, sf::Color color = sf::Color::White );
+							Graphics3D();
+							Graphics3D( sf::RenderTarget &theTarget);
+
+	void					SetRenderTarget( sf::RenderTarget &theTarget );
+	sf::RenderTarget *		GetRenderTarget();
+
+	void					DrawLine( Vector3f p1,  Vector3f p2, sf::Color color = sf::Color::White );
 
 protected:
-	virtual void		draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
 private:
-	Matrix4f			mViewTransform;
+	Vector3f				TransformPoint( const Vector3f &p );
+
+private:
+	sf::RenderTarget	  * mRenderTarget;
+	Matrix4f				mViewTransform;
 	
-}
+};
 
 
 #endif
--- src/Graphics3D.cpp
+++ src/Graphics3D.cpp
@@ -1,3 +1,84 @@
 #include "Graphics3D.h"
 
+#include <SFML/Graphics/VertexArray.hpp>
 
+#include "TransformUtils.h"
+
+
+Graphics3D::Graphics3D()
+	: mRenderTarget(0)
+{
+	TransformUtils::SetIdentity( mViewTransform );
+}
+
+
+
+
+
+Graphics3D::Graphics3D( sf::RenderTarget &theTarget )
+	: mRenderTarget(&theTarget)
+{
+
+}
+
+
+
+
+
+void Graphics3D::SetRenderTarget( sf::RenderTarget &theTarget )
+{
+	mRenderTarget = &theTarget;
+}
+
+
+
+
+
+sf::RenderTarget * Graphics3D::GetRenderTarget()
+{
+	return mRenderTarget;
+}
+
+
+
+
+
+void Graphics3D::DrawLine( Vector3f p1,  Vector3f p2, sf::Color color )
+{
+	if( mRenderTarget )
+	{
+		Vector3f outputP1 = TransformPoint( p1 );
+		Vector3f outputP2 = TransformPoint( p2 );
+
+		sf::VertexArray theLine;
+		theLine.setPrimitiveType( sf::PrimitiveType::Lines );
+		theLine.resize(2);
+		theLine[0] = sf::Vertex( sf::Vector2f(p1.x, p1.y),	color );
+		theLine[1] = sf::Vertex( sf::Vector2f(p2.x, p2.y),	color );
+
+		mRenderTarget->draw( theLine );
+	}
+}
+
+
+
+
+
+Vector3f Graphics3D::TransformPoint( const Vector3f &p )
+{
+	Vector3f theResult;
+	Vector4f hp;
+
+	hp.x = p.x;
+	hp.y = p.y;
+	hp.z = p.z;
+	hp.w = 1.0f;
+
+	mViewTransform * hp;
+
+	theResult.x = hp.x / hp.w;
+	theResult.y = hp.y / hp.w;
+	theResult.z = hp.z / hp.w;
+
+	return theResult;theResult.x = hp.x / hp.w;
+}
